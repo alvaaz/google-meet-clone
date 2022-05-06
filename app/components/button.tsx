0df20f5f0c9de02b79a392b535b94734576e72d9
alvaaz <alvaro@goede.cl>
2022-05-05 23:40:38 -0400

    create button component

---
+++ app/components/button.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+type ButtonProps = {
+  loading?: boolean;
+  children: React.ReactNode;
+  onClick?: () => void;
+  href?: string;
+  disabled?: boolean;
+  htmlType?: "button" | "submit" | "reset";
+  name?: string;
+  value?: string;
+};
+
+export default function Button({
+  loading,
+  children,
+  onClick,
+  href,
+  disabled,
+  htmlType,
+  name,
+  value,
+}: ButtonProps) {
+  return (
+    <button
+      htmlType={htmlType}
+      name={name}
+      value={value}
+      className="disabled:opacity-50 group relative w-full flex justify-center py-3 px-4 border border-transparent font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 shadow-sm mb-4"
+      disabled={disabled}
+    >
+      {loading && (
+        <svg
+          className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            strokeWidth="4"
+          ></circle>
+          <path
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          ></path>
+        </svg>
+      )}
+      {children}
+    </button>
+  );
+}
--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -11,6 +11,7 @@ import {
 } from "~/utils/validator.server";
 import { login, register, getUser } from "~/utils/auth.server";
 import { Form, Link, useActionData, useTransition } from "@remix-run/react";
+import Button from "~/components/button";
 
 export const loader: LoaderFunction = async ({ request }) => {
   return (await getUser(request)) ? redirect("/") : null;
@@ -156,37 +157,15 @@ export default function Login() {
             />
           </div>
 
-          <button
-            type="submit"
+          <Button
+            htmlType="submit"
             name="_action"
             value="login"
             disabled={processing}
-            className="disabled:opacity-50 group relative w-full flex justify-center py-3 px-4 border border-transparent font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 shadow-sm mb-4"
+            loading={processing}
           >
-            {processing && (
-              <svg
-                className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <circle
-                  className="opacity-25"
-                  cx="12"
-                  cy="12"
-                  r="10"
-                  stroke="currentColor"
-                  strokeWidth="4"
-                ></circle>
-                <path
-                  className="opacity-75"
-                  fill="currentColor"
-                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                ></path>
-              </svg>
-            )}
-            {processing ? "Iniciando sesión..." : "Iniciar sesión"}
-          </button>
+            Iniciar sesión
+          </Button>
         </Form>
         <p className="mt-2 text-center text-sm text-gray-600">
           ¿Aún no tienes una cuenta?{" "}
